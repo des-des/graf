166f5e61f18845fd3e0fb26eaeab939a27688959
eoin-mc-carthy <em1081@my.bristol.ac.uk>
2016-11-04 11:50:40 +0000

    basic empty list

--- test/list.test.js
+++ test/list.test.js
@@ -1,8 +1,26 @@
 const test = require('tape')
 
-const createList = require('../lib/list.js')
+const list = require('../lib/list.js')
 
-test('createList exists', t => {
-  t.equal(typeof createList, 'function')
+test('list exists', t => {
+  t.equal(typeof list, 'function')
+  t.end()
+})
+
+test('empty list', t => {
+  const emptyList = list()
+
+  t.equal(typeof emptyList, 'object', 'creates a list')
+  t.throws(
+    () => { emptyList.head() },
+    /Cannot call head on an empty list/,
+    'calling head on empty list throws'
+  )
+
+  t.throws(
+    () => { emptyList.tail() },
+    /Cannot call tail on an empty list/,
+    'calling tail on empty list throws'
+  )
   t.end()
 })
