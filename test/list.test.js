bdceeebc29dc6982410a3ea2acc867b4d83578cd
eoin-mc-carthy <em1081@my.bristol.ac.uk>
2016-11-05 14:33:41 +0000

    :heavy_plus_sign: add list with elems

--- test/list.test.js
+++ test/list.test.js
@@ -16,3 +16,20 @@ test('empty list', t => {
   t.equal(typeof emptyList.tail, 'undefined', 'has undefined tail')
   t.end()
 })
+
+test('list with one elem', t => {
+  const l = list(1)
+
+  t.equal(l.length, 1, 'has length 0')
+  t.equal(l.head, 1, 'has undefined head')
+  t.equal(l.tail.length, 0, 'has undefined tail')
+  t.end()
+})
+
+test('list with many elems', t => {
+  const l = list(0, 1, 2, 3)
+
+  t.equal(l.length, 4, 'has length 0')
+  t.equal(l.tail.tail.head, 2, 'can get nested elems')
+  t.end()
+})
